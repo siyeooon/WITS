1d5fe99199ed2cbe401b6c242b2902118af24cf4
yunmi099 <yunmi099@ajou.ac.kr>
2023-11-25 23:51:03 +0900

    context Api 예시 세팅

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -4,11 +4,13 @@ import * as router from './routes';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" Component={router.home}/>
-      <Route path="/ingame" Component={router.game}/>
-      <Route path="/ranking" Component={router.totalRanking}/>
-    </Routes>
+    <LoginProvider>
+      <Routes>
+        <Route path="/" Component={router.home}/>
+        <Route path="/ingame" Component={router.game}/>
+        <Route path="/ranking" Component={router.totalRanking}/>
+      </Routes>
+    </LoginProvider>
   );
 }
 
