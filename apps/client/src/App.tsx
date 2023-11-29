b25d3fe3c456fadde60ab52b80259beb79dc5111
Jin Hyeok Son <thswlsgur98@gmail.com>
2023-11-30 05:58:26 +0900

    dockerize

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import * as router from './routes';
-import { LoginProvider } from './context/loginContext';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import * as router from "./routes";
+import { LoginProvider } from "./context/loginContext";
 
 function App() {
   return (
     <LoginProvider>
       <Routes>
-        <Route path="/" Component={router.home}/>
-        <Route path="/ingame" Component={router.game}/>
-        <Route path="/ranking" Component={router.totalRanking}/>
+        <Route path="/" Component={router.home} />
+        <Route path="/ingame" Component={router.game} />
+        <Route path="/ranking" Component={router.totalRanking} />
       </Routes>
     </LoginProvider>
   );
