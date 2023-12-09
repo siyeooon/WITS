841275e47545dffb04f93e0e24831f35d271f2ac
yunmi099 <yunmi099@ajou.ac.kr>
2023-12-10 02:24:39 +0900

    feat: header에서 트로피 클릭시 전체 링크로 이동

--- apps/client/src/components/header/index.tsx
+++ apps/client/src/components/header/index.tsx
@@ -1,5 +1,7 @@
+import { useNavigate } from 'react-router-dom'
 import styles from './styles.module.scss'
 export const Header = ()=>{
+  const navigate = useNavigate();
     return(       
     <div className={styles.header}>
         <div className={styles.logo}>WITS</div>
@@ -7,6 +9,7 @@ export const Header = ()=>{
             <img 
               src='/src/assets/ingame/ranking.png'
               style={{width:50, height: 50, margin:5}}
+              onClick={()=>navigate('/ranking')}
             />
             <img 
                 src='/src/assets/ingame/profile.png'
